1118c8f247a4d0e6885ec01343bd6dd3ec7a6f0c
João Farias <you@example.com>
2020-12-08 17:24:59 +0000

    Implementação da funcionalidade de criar um utilizador

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
+    session_start();
 
-    $var = "Hello World";
-    echo $var;
+    $url_parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+    $controllers = [
+        "access",
+    ];
+
+    $controller = "access";
+
+    if( !empty($url_parts[1]) ) {
+
+        if( !in_array($url_parts[1], $controllers) ) {
+            header("HTTP/1.1 404 Not Found");
+            die("404 Not Found");
+        }
+        
+        $controller = $url_parts[1];
+    }
+
+    if( isset($url_parts[2]) ) {
+        $action = $url_parts[2];
+    }
+
+    require("controller/" .$controller. ".php")
 
 ?>
